9b1b57fb9f0dc22e94034ec1aa9d0f8c1dbeb303
thoavu19 <thoavu222@gmail.com>
2022-03-22 21:43:29 +0700

    thoa

---
+++ hdt.cpp
@@ -0,0 +1,51 @@
+#include <iostream.h>
+class  soPhuc{
+	private:
+		int pt, pa;
+	public:
+		void Nhap(){
+			
+				cout <<"Nhap phan thuc: ";
+				cin >> pt;
+				cout <<"Nhap phan ao: ";
+				cin >> pa;
+			
+		}
+		void Xuat(){
+			cout <<"\nA = "<<pt<<" + "<<" *j"<<endl;
+		}
+		int getPT(){
+			return pt;
+		}
+		int getPA(){
+			return pa;
+		}
+};
+int main(int argc, char *argv[])
+{
+	soPhuc  a[2];
+	for(int i = 0; i < 2; i++){
+		a[i].Nhap();
+	}
+	cout <<"\nTong 2 so phuc la: "
+		 << a[0].getPT() + a[1].getPT() 
+		 << " + " 
+		 << a[0].getPA() + a[1].getPA()
+		 << " *j";   
+	cout <<"\nHieu 2 so phuc la: "
+		 << a[0].getPT() - a[1].getPT() 
+		 << " + (" 
+		 << a[0].getPA() - a[1].getPA()
+		 << ") *j";   
+	cout <<"\nTich 2 so phuc la: "
+		 << a[0].getPT() * a[1].getPT() 
+		 << " + " 
+		 << a[0].getPA() * a[1].getPA()
+		 << " *j";   
+	cout <<"\nThuong 2 so phuc la: "
+		 << a[0].getPT() / a[1].getPT() 
+		 << " + " 
+		 << a[0].getPA() / a[1].getPA()
+		 << " *j";                                  
+	return 0;
+}
